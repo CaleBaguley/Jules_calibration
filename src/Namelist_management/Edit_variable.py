002341d779bd1449e278918721c7779753d8b9cf
CaleBaguley <jb14389@bristol.ac.uk>
2024-12-11 12:12:51 +0000

    Added functions to edit soil variable and writ all soil variables.

--- src/Namelist_management/Edit_variable.py
+++ src/Namelist_management/Edit_variable.py
@@ -1,4 +1,4 @@
-
+import src.Namelist_management.Read as Read
 """
 Code to open a namelist file and edit a single input variable
 """
@@ -48,5 +48,54 @@ def edit_variable(file_address, namelist, variable, value):
     print("Writing changes to file...")
     with open(file_address, "w") as file:
         file.writelines(lines)
+
+    return True
+
+def edit_soil_variable(file_address, variable, new_value, ancillary_file):
+    """
+    Edits the value of a variable in a soil file
+    :param file_address: Address of the file (str)
+    :param variable: Variable to edit (str)
+    :param new_value: Value to set the variable to (str)
+    :return: True if the variable was edited, False otherwise
+    """
+
+    # Get the soil variable names
+    soil_variables = Read.read_soil_variable_names(ancillary_file)
+
+    # Check if the variable is in the soil file
+    if variable not in soil_variables:
+        print("Variable not found.")
+        return False
+
+    # Get the position of the variable to change
+    variable_index = soil_variables.index(variable)
+
+    # Get the soil variables
+    soil_values = Read.read_soil_variable_values(file_address)
+
+    # Change the value
+    soil_values[variable_index] = new_value
+
+    # Write the changes to the file
+    write_soil_variables(file_address, soil_values)
+
+    return True
+
+def write_soil_variables(file_address, soil_values):
+    """
+    Writes all the soil variables to a soil file
+    :param file_address: Address of the file (str)
+    :param soil_values: Values of the soil variables (list)
+    :return: True if the variables were written, False otherwise
+    """
+
+    # Convert the list of values to a string
+    string = " ".join(soil_values)
+
+    # Write the changes to the file
+    print("Writing changes to file...")
+    with open(file_address, "w") as file:
+        file.write(string)
 
     return True
