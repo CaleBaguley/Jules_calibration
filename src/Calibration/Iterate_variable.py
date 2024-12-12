3de2e763d2a91d636b65c489dde0c81296ac4ef2
CaleBaguley <jb14389@bristol.ac.uk>
2024-12-12 11:27:26 +0000

    Moved soil ancillary duplication to Duplicate.py

--- src/Calibration/Iterate_variable.py
+++ src/Calibration/Iterate_variable.py
@@ -130,3 +130,123 @@ def iterate_variable(jules_executable_address,
     os.rmdir(os.getcwd() + "/tmp")
 
     return
+
+def iterate_soil_variable(jules_executable_address,
+                          master_namelist_address,
+                          soil_ancillary_address,
+                          variable_name,
+                          variable_values,
+                          output_folder,
+                          run_ids,
+                          keep_dump_files = False,
+                          overwrite_tmp_files = False):
+
+    """
+    Iterate over a series of values for a given soil variable
+    :param jules_executable_address: file address of the JULES executable (str)
+    :param master_namelist_address: folder containing the namelists to copy (str)
+    :param soil_ancillary_address: soil ancillary file to use (str)
+    :param variable_name: variable to iterate over (str)
+    :param variable_values: values to iterate over (list of str)
+    :param output_folder: JULES output folder (str)
+    :param run_ids: name of each run (list of str)
+    :param keep_dump_files: save JULES dumpfiles (bool)
+    :param overwrite_tmp_files: overwrite any existing tmp files (bool)
+    :return:
+    """
+
+    # Make a coppy of the master namelist to edit
+    tmp_namelist = os.getcwd() + ("/tmp/namelist/")
+    Duplicate.duplicate(master_namelist_address, tmp_namelist, overwrite=overwrite_tmp_files)
+
+    # Make a copy of the soil ancillary file
+    Duplicate.
+
+    # Create a temporary output folder
+    tmp_output = os.getcwd() + "/tmp/output/"
+    if (os.path.exists(tmp_output)):
+        if overwrite_tmp_files:
+            # Walk through the directory and delete all files
+            for root, dirs, files in os.walk(tmp_output, topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+                for name in dirs:
+                    os.rmdir(os.path.join(root, name))
+            os.rmdir(tmp_output)
+        else:
+            exception("ERROR: temporary output folder already exists.\n"
+                      + "Please delete the folder or set overwrite_tmp_files = True.\n")
+    os.mkdir(tmp_output)
+
+    # Change the JULES output to a temporary folder
+    # Note we need to add ' to both ends of the output directory.
+    Edit_variable.edit_variable(tmp_namelist + "output.nml",
+                                "jules_output",
+                                "output_dir",
+                                "'" + tmp_output + "'")
+
+    # Check the actual output folder exists
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Get the output profile name
+    profile_name = Read.read_variable(tmp_namelist + "output.nml",
+                                      "jules_output_profile",
+                                      "profile_name")
+
+    # Remove any quotes from the profile name
+    profile_name = profile_name.strip("'")
+    profile_name = profile_name.strip('"')
+
+    # Iterate over the values
+    for i, value in enumerate(variable_values):
+
+        # Edit the variable
+        Edit_variable.edit_variable(tmp_namelist + variable_namelist_file,
+                                    variable_namelist,
+                                    variable_name,
+                                    value)
+
+        # Set the current run id
+        current_run_id = run_ids[i]
+
+        Edit_variable.edit_variable(tmp_namelist + "output.nml",
+                                    "jules_output",
+                                    "run_id",
+                                    "'" + current_run_id + "'")
+
+        # Run JULES
+        Run_JULES.run_JULES(jules_executable_address, tmp_namelist)
+
+        # Move the output from the temporary folder to the output folder
+        os.rename(tmp_output + current_run_id + "." + profile_name + ".nc",
+                  output_folder + current_run_id + "." + profile_name + ".nc")
+
+        # If user wants to keep dump files, move them to the output folder
+        if keep_dump_files:
+
+            # Make a folder for the dump files
+            current_dump_folder = output_folder + "/" + current_run_id + "_dump/"
+            os.mkdir(current_dump_folder)
+
+            output_files = os.listdir(tmp_output)
+            for file in output_files:
+                if 'dump' in file:
+                    os.rename(tmp_output + file, current_dump_folder + file)
+
+        # Delete the temporary output folder contents
+        output_files = os.listdir(tmp_output)
+        for file in output_files:
+            os.remove(tmp_output + file)
+
+    # Delete the temporary output folder
+    os.rmdir(tmp_output)
+
+    # Delete the temporary namelist folder
+    output_files = os.listdir(tmp_namelist)
+    for file in output_files:
+        os.remove(tmp_namelist + file)
+    os.rmdir(tmp_namelist)
+
+    # Remove tmp folder
+    os.rmdir(os.getcwd() + "/tmp")
--- src/Namelist_management/Duplicate.py
+++ src/Namelist_management/Duplicate.py
@@ -1,5 +1,7 @@
 import os
 
+from src.Namelist_management import Edit_variable as Edit_variable
+
 """
 Contains code used to duplicate the contents of a folder containing namelist files
 """
@@ -65,3 +67,32 @@ def duplicate_file(file_address, duplicate_address, overwrite=False):
     os.system("cp " + file_to_copy + " " + duplicate_address_terminal)
 
     return True
+
+
+def duplicate_soil_ancillary(soil_ancillary_file, duplicate_address, ancillary_namelist, overwrite=False):
+    """
+    Duplicates the soil ancillary files to the duplicate_address
+    :param soil_ancillary_file: Soil ancillary files to duplicate (str)
+    :param duplicate_address: Address to copy the soil ancillary files to (str)
+    :param ancillary_namelist: Namelist containing the soil ancillary file address (str)
+    :param overwrite = False: If True, overwrites the files in the duplicate_address (bool)
+    :return: Address of the new soil ancillary file (str)
+    """
+
+    # Duplicate the soil ancillary file
+    if not duplicate_file(soil_ancillary_file,
+                          duplicate_address,
+                          overwrite):
+        return None
+
+    # Need to calculate the new soil ancillary file address
+    new_soil_file = duplicate_address + soil_ancillary_file.split("/")[-1]
+
+    # Update the soil ancillary file address in the ancillary namelist
+    if not Edit_variable.edit_variable(ancillary_namelist,
+                                       "jules_soil_props",
+                                       "file",
+                                       new_soil_file):
+        return False
+
+    return new_soil_file
