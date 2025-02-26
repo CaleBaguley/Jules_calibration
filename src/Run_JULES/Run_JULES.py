e7bc757c0d507808d44f74d0054059d770c51c03
CaleBaguley <jb14389@bristol.ac.uk>
2025-02-26 13:41:56 +0000

    Added option to save the terminal output of JUELS into a file.

--- src/Run_JULES/Run_JULES.py
+++ src/Run_JULES/Run_JULES.py
@@ -1,14 +1,16 @@
 import os
+import subprocess
 
 """
 Code to run JULES from python
 """
 
-def run_JULES(jules_executable_address, namelist_folder_address):
+def run_JULES(jules_executable_address, namelist_folder_address, terminal_output_address=None):
     """
     Run JULES from python
-    :param jules_executable_address: Address of JULES executable
-    :param namelist_folder_address: Address of the folder containing the namelists
+    :param jules_executable_address: Address of JULES executable (str)
+    :param namelist_folder_address: Address of the folder containing the namelists (str)
+    :param terminal_output_address: Address of the file to write the terminal output to (str) (optional)
     """
 
     # Get the current working directory
@@ -18,7 +20,11 @@ def run_JULES(jules_executable_address, namelist_folder_address):
     os.chdir(namelist_folder_address)
 
     # Run JULES
-    os.system(jules_executable_address)
+    if(terminal_output_address is not None):
+        with open(terminal_output_address, "w") as f:
+            subprocess.run(jules_executable_address, stdout=f)
+    else:
+        os.system(jules_executable_address)
 
     # cd back to the original directory
     os.chdir(cwd)
