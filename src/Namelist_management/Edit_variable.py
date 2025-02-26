899a1bf8c8309fae3c909cd31457b8b880b7af7a
CaleBaguley <jb14389@bristol.ac.uk>
2025-02-26 13:20:35 +0000

    Fixed bug is variable is the last in the name list. Added verbose flag for writing to terminal.

--- src/Namelist_management/Edit_variable.py
+++ src/Namelist_management/Edit_variable.py
@@ -3,13 +3,14 @@ import src.Namelist_management.Read as Read
 Code to open a namelist file and edit a single input variable
 """
 
-def edit_variable(file_address, namelist, variable, value):
+def edit_variable(file_address, namelist, variable, value, verbose = False):
     """
     Edits the value of a variable in a namelist file
     :param file_address: Address of the file (str) or list of addresses (list of str)
     :param namelist: Namelist to edit (str) or list of namelists to edit (list of str)
     :param variable: Variable to edit (str) or list of variables to edit (list of str)
     :param value: Value to set the variable to (str) or list of values to set the variables to (list of str)
+    :param verbose = False: If True, prints the changes made (bool)
     :return: True if the variable was edited, False otherwise
     """
 
@@ -32,38 +33,47 @@ def edit_variable(file_address, namelist, variable, value):
             namelist_start_index = i
             break
     else:
-        print("Namelist not found.")
+        print(f"Namelist ({namelist}) not found.")
         return False
 
     # Find the variable
     for i, line in enumerate(lines[namelist_start_index+1:]):
         # Check if we have reached the next namelist
         if "&" == line[0]:
-            print("Variable not found.")
+            print(f"Variable ({variable}) not found.")
             return False
         # Check if the variable is in the line
         elif variable in line:
-            print("Variable found.")
-            lines[namelist_start_index+1 + i] = variable + "=" + value + ",\n"
+            print(f"Variable ({variable}) found.")
+            lines[namelist_start_index+1 + i] = variable + "=" + value
             break
     # If the variable was not found
     else:
-        print("Variable not found.")
+        print(f"Variable ({variable}) not found.")
         return False
 
+    # Need to account for the case where the variable is the last in the namelist
+    if lines[namelist_start_index+1 + i + 1] == "/\n":
+        lines[namelist_start_index+1 + i] += "\n"
+    else:
+        lines[namelist_start_index+1 + i] += ",\n"
+
     # Write the changes to the file
-    print("Writing changes to file...")
+    if verbose:
+        print("Writing changes to file...")
     with open(file_address, "w") as file:
         file.writelines(lines)
 
     return True
 
-def edit_soil_variable(file_address, variables, new_values, ancillary_file):
+def edit_soil_variable(file_address, variables, new_values, ancillary_file, verbose = False):
     """
     Edits the value of a variable in a soil file
     :param file_address: Address of the file (str)
     :param variables: Variable to edit (str) or list of variables to edit (list of str)
     :param new_value: Value to set the variable to (str) or list of values to set the variables to (list of str)
+    :param ancillary_file: Address of the ancillary file (str)
+    :param verbose = False: If True, prints the changes made (bool)
     :return: True if the variable was edited, False otherwise
     """
 
@@ -94,15 +104,16 @@ def edit_soil_variable(file_address, variables, new_values, ancillary_file):
         soil_values[variable_indecies[i]] = new_values[i]
 
     # Write the changes to the file
-    write_soil_variables(file_address, soil_values)
+    write_soil_variables(file_address, soil_values, verbose)
 
     return True
 
-def write_soil_variables(file_address, soil_values):
+def write_soil_variables(file_address, soil_values, verbose = False):
     """
     Writes all the soil variables to a soil file
     :param file_address: Address of the file (str)
     :param soil_values: Values of the soil variables (list)
+    :param verbose = False: If True, prints the changes made (bool)
     :return: True if the variables were written, False otherwise
     """
 
@@ -110,7 +121,8 @@ def write_soil_variables(file_address, soil_values):
     string = " ".join(soil_values)
 
     # Write the changes to the file
-    print("Writing changes to file...")
+    if(verbose):
+        print("Writing changes to file...")
     with open(file_address, "w") as file:
         file.write(string)
 
